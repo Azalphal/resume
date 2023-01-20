644a661af9b49ea97cbb1dfd103e33568cbc3651
Bouyou34 <bouyou-mael@live.fr>
2023-01-20 12:26:38 +0100

    update the parallex effect for it to finally work

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,9 +1,32 @@
-const menu = document.getElementById("menu")
-const text = document.getElementsByClassName("menu__link")
+// window.addEventListener('load', () => {
+//     const menu = document.getElementById("menu")
+//     const text = document.querySelectorAll(".menu__link");
 
-Array.from(text)
-    .forEach((item, index) => {
-        item.onmouseover = () => {
-            menu.style.setProperty("--active-index", index)
-        }
-    });
+//     console.log(menu, text)
+
+//     Array.from(text)
+//         .forEach((item, index) => {
+//             item.onmouseover = () => {
+//                 menu.style.setProperty("--active-index", index)
+//                 console.log(`Hovered over item ${index}`)
+//             }
+//         });
+// });
+
+window.addEventListener('load', () => {
+    // const menu = document.getElementById("menu");
+    const text = document.querySelectorAll(".menu__link");
+    const menuBackground = document.querySelector("#menu--background");
+    const menuBackgroundImage = document.querySelector("#menu--background--image");
+
+    console.log(menuBackground, menuBackgroundImage, text)
+
+    Array.from(text)
+        .forEach((item, index) => {
+            item.onmouseover = () => {
+                menuBackground.style.setProperty("--active-index", index)
+                menuBackgroundImage.style.setProperty("--active-index", index)
+                console.log(`Hovered over item ${index}`)
+            }
+        });
+});
