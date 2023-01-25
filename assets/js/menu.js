e86a944b4e1f8f6263dc2461ad9220c1a922db9a
Bouyou34 <bouyou-mael@live.fr>
2023-01-25 16:31:31 +0100

    remove debugging loggers

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,15 +3,11 @@ window.addEventListener('load', () => {
     const menuBackground = document.querySelector("#menu--background");
     const menuBackgroundImage = document.querySelector("#menu--background--image");
 
-    console.log(`Correctly loaded the background pattern in "${menuBackground}"`,`\nCorrectly loaded the background image in "${menuBackgroundImage}"`, `\nCorrectly loaded the text elements below:`)
-    console.dir(text)
-
     Array.from(text)
         .forEach((item, index) => {
             item.onmouseover = () => {
                 menuBackground.style.setProperty("--active-index", index)
                 menuBackgroundImage.style.setProperty("--active-index", index)
-                console.log(`Hovered over item ${index}`)
             }
         });
 });
